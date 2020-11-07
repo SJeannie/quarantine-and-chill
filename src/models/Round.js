9b91d2d68de8e79213a089e9d73ef9766dbb2b83
Jeannie Kim <sj.jeannie.kim@gmail.com>
2020-11-07 13:17:21 -0600

    Timer-Round association

--- src/models/Round.js
+++ src/models/Round.js
@@ -1,12 +1,15 @@
-import { Model, string, integer, include, hasMany } from '@triframe/scribe';
+import { Model, integer, include, hasMany } from '@triframe/scribe';
 import { Resource } from '@triframe/core';
 
 export class Round extends Resource {
     @include(Model)
 
     @integer
-    time = 0  // intitial time remaing at start ??? 5 / 10 / 15
+    time = 0  // initial time remaining at start ??? 5 / 10 / 15
 
 	@hasMany({ as : 'round'})
-	users = []
+    users = []
+
+    @hasOne
+	timer = null
 }
