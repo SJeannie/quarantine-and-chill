af91a1a01e28e0ffb36fd93a8d5e280e063d65dd
Jeannie Kim <sj.jeannie.kim@gmail.com>
2020-11-07 11:39:49 -0600

    User-Round-Rank associations

--- src/App.js
+++ src/App.js
@@ -6,29 +6,6 @@ import { Matching } from './views/Matching'
 import { NavBar } from './views/NavBar'
 import { Ticker } from './views/Ticker'
 
-const UserDetails = tether(function* ({ Api }) {
-	const { User } = Api
-	const [ user ] = yield User.list(`
-	name,
-	pets {
-	name
-	}
-	`)
-	return (
-			<div>
-					<h1>{user.name}</h1>
-					<ul>
-							{user.pets.map(pet => (
-									<li>
-											<span>{pet.name} </span>
-											<button onClick={() => pet.delete()}>Delete</button>
-									</li>
-							))}
-					</ul>
-			</div>
-	)
-})
-
 export default () => (
 	<Provider url={process.env.REACT_APP_BACKEND_URL}>
 		<Route exact path='/login' component={Login} />
