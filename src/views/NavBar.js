6ced69a4c8636b6dfc0ee367647aca13560ae8f6
corymharper <zepher2211@gmail.com>
2020-11-07 16:45:42 -0600

    navbar styles refactored

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -1,33 +1,53 @@
 import React from 'react';
 import {
 	tether,
-	Container,
 	Appbar,
-    BubbleButton,
     Heading,
-	Subheading,
-	Area
 } from '@triframe/designer';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+
+const SignOutButton = styled.button`
+	border-radius: 3px;
+	background-color: #cb4e4e;
+	color: white;
+	margin-left: auto;
+	margin-right: 1em;
+	border: none;
+	box-shadow: -3px 0 #ab3c3c;
+
+	&:hover {
+		box-shadow: -2px 0 #ab3c3c;
+    	left: -1px;
+	}
+`;
+
+const HeaderContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-top: ${props => props.userExists ? '1em' : '0'};
+`;
 
 export const NavBar = tether(function* ({ Api: { User }, redirect }) {
 	const user = yield User.current();
 	
 	return (
 		<Appbar inline>
-			<Heading>Quarantine and Chill</Heading>
+			<HeaderContainer userExists={!!user}>
+				<Heading>Quarantine and Chill</Heading>
+				{user && <em style={{paddingBottom: '1.25em'}}>Hey {user.username}!</em>}
+			</HeaderContainer>
 			{user && (
-				<>
-					<Subheading>{user.username}</Subheading>
-					<BubbleButton
-						onPress={async () => {
+					<SignOutButton
+						onClick={async () => {
 						await User.logout();
 
 						redirect('/login');
 						}}
 					>
-						Log out
-					</BubbleButton>
-				</>
+						<FontAwesomeIcon icon={faSignOutAlt} />
+					</SignOutButton>
 			)}
 		</Appbar>
 	);
