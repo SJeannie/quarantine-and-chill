dd435f512b1cd0512407d94d68338fa80475158b
Joseph-Burks <joe_burks_3@yahoo.com>
2020-11-08 01:48:22 -0600

    routing adjustment

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from 'react'
 import { Provider, Route, tether, Container } from '@triframe/designer'
 //import { Chat } from './views/Chat'
 import { Login } from './views/Login';
-import { Matching } from './views/Matching'
+import { Main } from './views/Main'
 import { NavBar } from './views/NavBar'
-import { Ticker } from './views/Ticker'
 import GlobalStyle from './theme';
-import Evolution from './views/evolution';
 
 export default () => (
 	<Provider url={process.env.REACT_APP_BACKEND_URL}>
@@ -14,8 +12,7 @@ export default () => (
 		<Container>
 			<NavBar />
 			<Route exact path='/login' component={Login} />
-			<Route exact path='/' component={Matching} />
-			<Route exact path='/evolution' component={Evolution} />
+			<Route exact path='/' component={Main} />
 		</Container>
 	</Provider>
 );
---
+++ src/views/Main.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { tether, Column, Grid, Container } from '@triframe/designer';
+import { Ticker } from './Ticker';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+import Evolution from './evolution';
+
+const MatchingContainer = styled.div`
+	margin: auto;
+	padding-top: 30%;
+	font-size: 5em;
+`;
+
+export const Main = tether(function* ({ Api, redirect, session }) {
+	let { User } = Api;
+	let user = yield User.current(`*, rank {*}, round {*}`);
+
+	if (user) {
+		return (
+			<Container>
+				<Grid base={7}>
+					<Column xs={5}>
+						{user.round.isFull ? (
+							<Evolution user={user}></Evolution>
+						) : (
+							<MatchingContainer>
+								<FontAwesomeIcon icon={faCircleNotch} spin />
+								&nbsp; matching…
+							</MatchingContainer>
+						)}
+					</Column>
+					<Column xs={2}>
+						<Ticker />
+					</Column>
+				</Grid>
+			</Container>
+		);
+	} else {
+		redirect('/login');
+	}
+});
