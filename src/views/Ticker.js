8eca75b35154cfbe266d3b868aee1c55090c2a82
Joseph-Burks <joe_burks_3@yahoo.com>
2020-11-07 23:15:26 -0600

    Ticker and README

--- src/views/Ticker.js
+++ src/views/Ticker.js
@@ -2,82 +2,60 @@ import React from 'react';
 import {
 	tether,
 	Container,
-    Card,
-    Area,
-    Button,
-    Heading,
-    Subheading,
-    Grid,
-    Column,
-    Divider,
-    Avatar,
-    Icon,
-    Section
+	Card,
+	Area,
+	Button,
+	Heading,
+	Subheading,
+	Grid,
+	Column,
+	Divider,
+	Avatar,
+	Icon,
+	Section,
+	Paragraph,
 } from '@triframe/designer';
 
 export const Ticker = tether(function* ({ Api, redirect }) {
+	const { User, Rank } = Api;
 
-	const { User } = Api
+	let users = yield User.list(`
+        *,
+        rank {
+            *
+        }
+    `);
+
+	console.log(users);
 
 	return (
 		<Container>
 			<Heading>Players</Heading>
-			{/* {User.list.map(user => {    // couldn't get the map working vvv card for each user
-                return(
-                    <Section>
-                        <Card>
-                            <Grid base={3}>
-                                <Column>
-                                    <Heading>user.name</Heading>
-                                    <Divider></Divider>
-                                    <Subheading>user.rank.title</Subheading>
-                                </Column>
-                                <Column>
-                                    <Avatar.Image />
-                                </Column>
-                                <Column>
-                                    <Icon />
-                                </Column>
-                            </Grid>
-                        </Card>
-                    </Section>
-                )
-            })} */}
-
-			<Section>
-				<Card>
-					<Grid base={3}>
-						<Column>
-							<Heading>user.name</Heading>
-							<Divider></Divider>
-							<Subheading>user.rank.title</Subheading>
-						</Column>
-						<Column>
-							<Avatar.Image />
-						</Column>
-						<Column>
-							<Icon />
-						</Column>
-					</Grid>
-				</Card>
-			</Section>
-			<Section>
-				<Card>
-					<Grid base={3}>
-						<Column>
-							<Heading>user.name</Heading>
-							<Divider></Divider>
-							<Subheading>user.rank.title</Subheading>
-						</Column>
-						<Column>
-							<Avatar.Image />
-						</Column>
-						<Column>
-							<Icon />
-						</Column>
-					</Grid>
-				</Card>
-			</Section>
+			{users.map(user => {
+				return (
+					<Section>
+						<Card>
+							<Grid base={9}>
+								<Column xs={4} sm={4} md={4} lg={4} xl={4}>
+									<Avatar.Image />
+								</Column>
+								<Column xs={4} sm={4} md={4} lg={4} xl={4}>
+									<Subheading>{user.username}</Subheading>
+									<Divider></Divider>
+									<Paragraph>{user.rank.title}</Paragraph>
+								</Column>
+								<Column>
+									{user.isWinner ? (
+										<Icon name='arrow-up-circle-outline' />
+									) : (
+										<Icon name='arrow-down-circle' />
+									)}
+								</Column>
+							</Grid>
+						</Card>
+					</Section>
+				);
+			})}
 		</Container>
 	);
 });
